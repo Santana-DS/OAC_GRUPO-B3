91eb1858279aefa047ad209b3d7b331f3bf27c03
Santana-DS <lucassantanasilva.lss@gmail.com>
2025-06-22 23:08:37 -0300

    commit

--- Laboratorio_3-CPU_RISC-V_MULTICICLO/TopDE_restored/Controlador.vhd
+++ Laboratorio_3-CPU_RISC-V_MULTICICLO/TopDE_restored/Controlador.vhd
@@ -11,7 +11,6 @@ entity Controlador is
         zero          : in  std_logic;
         EscreveIR     : out std_logic;
         IouD          : out std_logic;
-        EscrevePCCond : out std_logic;
         EscrevePC     : out std_logic;
         OrigPC        : out std_logic;
         ALUOp         : out std_logic_vector(1 downto 0);
@@ -22,18 +21,20 @@ entity Controlador is
         EscreveMem    : out std_logic;
         EscreveA      : out std_logic;
         EscreveB      : out std_logic;
-        EscreveSaidaULA : out std_logic
+        EscreveSaidaULA : out std_logic;
+        -- A porta EscrevePCCond não é mais necessária nesta lógica otimizada
+        EscrevePCCond : out std_logic
     );
 end Controlador;
 
-architecture fsm_optimized of Controlador is
+architecture fsm_corrected of Controlador is
     type state_type is (
         ST_FETCH_1, ST_FETCH_2, ST_DECODE, 
         ST_EXEC_R, ST_ALU_WB,
         ST_MEM_ADDR, ST_MEM_READ_1, ST_MEM_READ_2, ST_MEM_WB,
         ST_MEM_WRITE_1, ST_MEM_WRITE_2,
-        ST_BEQ_EXEC, ST_JAL_EXEC, ST_JALR_EXEC,
-        ST_PC_UPDATE
+        ST_BEQ_EXEC, ST_JUMP_EXEC,
+        ST_ADDR_CALC, ST_PC_UPDATE
     );
     signal pr_state, nx_state : state_type;
 
@@ -49,7 +50,7 @@ begin
 
     process(pr_state, opcode, zero)
     begin
-        EscreveIR <= '0'; IouD <= '0'; EscrevePCCond <= '0'; EscrevePC <= '0';
+        EscreveIR <= '0'; IouD <= '0'; EscrevePC <= '0'; EscrevePCCond <= '0';
         OrigPC <= '0'; ALUOp <= "00"; OrigAULA <= '0'; OrigBULA <= "00";
         EscreveReg <= '0'; Mem2Reg <= '0'; EscreveMem <= '0';
         EscreveA <= '0'; EscreveB <= '0'; EscreveSaidaULA <= '0';
@@ -62,43 +63,33 @@ begin
 
             when ST_FETCH_2 =>
                 EscreveIR <= '1';
-                EscrevePC <= '1';
-                OrigPC <= '0';
-                ALUOp <= "00";
-                OrigAULA <= '0';
-                OrigBULA <= "01";
+                EscrevePC <= '1'; OrigPC <= '0';
+                OrigAULA <= '0'; OrigBULA <= "01"; ALUOp <= "00";
                 EscreveSaidaULA <= '1';
                 nx_state <= ST_DECODE;
 
             when ST_DECODE =>
-                EscreveA <= '1';
-                EscreveB <= '1';
+                EscreveA <= '1'; EscreveB <= '1';
                 case opcode is
-                    when OPC_RTYPE => nx_state <= ST_EXEC_R;
-                    when OPC_ITYPE_L => nx_state <= ST_MEM_ADDR;
-                    when OPC_STYPE => nx_state <= ST_MEM_ADDR;
+                    when OPC_RTYPE | OPC_ITYPE_I => nx_state <= ST_EXEC_R;
+                    when OPC_ITYPE_L | OPC_STYPE => nx_state <= ST_MEM_ADDR;
                     when OPC_BTYPE => nx_state <= ST_BEQ_EXEC;
-                    when OPC_JTYPE => nx_state <= ST_JAL_EXEC;
-                    when OPC_ITYPE_J => nx_state <= ST_JALR_EXEC;
-                    when OPC_ITYPE_I => nx_state <= ST_EXEC_R;
+                    when OPC_JTYPE | OPC_ITYPE_J => nx_state <= ST_JUMP_EXEC;
                     when others => nx_state <= ST_FETCH_1;
                 end case;
 
             when ST_EXEC_R =>
-                OrigAULA <= '1';
+                OrigAULA <= '1'; ALUOp <= "10";
                 if (opcode = OPC_RTYPE) then
                     OrigBULA <= "00";
-                    ALUOp <= "10";
                 else
                     OrigBULA <= "10";
-                    ALUOp <= "00";
                 end if;
                 EscreveSaidaULA <= '1';
                 nx_state <= ST_ALU_WB;
             
             when ST_ALU_WB =>
-                EscreveReg <= '1';
-                Mem2Reg <= '0';
+                EscreveReg <= '1'; Mem2Reg <= '0';
                 nx_state <= ST_FETCH_1;
 
             when ST_MEM_ADDR =>
@@ -111,8 +102,7 @@ begin
                 end if;
             
             when ST_MEM_READ_1 =>
-                IouD <= '1';
-                nx_state <= ST_MEM_READ_2;
+                IouD <= '1'; nx_state <= ST_MEM_READ_2;
             
             when ST_MEM_READ_2 =>
                 nx_state <= ST_MEM_WB;
@@ -122,8 +112,7 @@ begin
                 nx_state <= ST_FETCH_1;
                 
             when ST_MEM_WRITE_1 =>
-                IouD <= '1'; EscreveMem <= '1';
-                nx_state <= ST_MEM_WRITE_2;
+                IouD <= '1'; EscreveMem <= '1'; nx_state <= ST_MEM_WRITE_2;
                 
             when ST_MEM_WRITE_2 =>
                 nx_state <= ST_FETCH_1;
@@ -131,22 +120,22 @@ begin
             when ST_BEQ_EXEC =>
                 OrigAULA <= '1'; OrigBULA <= "00"; ALUOp <= "01";
                 if (zero = '1') then
-                    OrigAULA <= '0'; OrigBULA <= "10"; ALUOp <= "00";
-                    EscreveSaidaULA <= '1';
-                    nx_state <= ST_PC_UPDATE;
+                    nx_state <= ST_ADDR_CALC;
                 else
                     nx_state <= ST_FETCH_1;
                 end if;
-                
-            when ST_JAL_EXEC =>
+            
+            when ST_JUMP_EXEC =>
                 EscreveReg <= '1'; Mem2Reg <= '0';
-                OrigAULA <= '0'; OrigBULA <= "10"; ALUOp <= "00";
-                EscreveSaidaULA <= '1';
-                nx_state <= ST_PC_UPDATE;
+                nx_state <= ST_ADDR_CALC;
 
-            when ST_JALR_EXEC =>
-                EscreveReg <= '1'; Mem2Reg <= '0';
-                OrigAULA <= '1'; OrigBULA <= "10"; ALUOp <= "00";
+            when ST_ADDR_CALC =>
+                if (opcode = OPC_ITYPE_J) then -- JALR
+                    OrigAULA <= '1';
+                else -- BEQ, JAL
+                    OrigAULA <= '0';
+                end if;
+                OrigBULA <= "10"; ALUOp <= "00";
                 EscreveSaidaULA <= '1';
                 nx_state <= ST_PC_UPDATE;
                 
@@ -156,4 +145,4 @@ begin
                 
         end case;
     end process;
-end fsm_optimized;
+end fsm_corrected;
